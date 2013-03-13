9a76c45e2c9c1645bd004d083c2b4ec7199dd0fe
Emilian <bloodb0ne@yahoo.co.uk>
2013-03-13 15:22:54 +0200

    More testing.
    Started to test more parsing ideas,but im starting to think that a
    interative parser should do a better job.

--- basic-checks.clj
+++ basic-checks.clj
@@ -29,3 +29,34 @@
 	(re-seq #"\(defn[\:\w\[\]\(\p{Space})]+" test-string-def))
 
 (rest def-test)
+
+
+;test 2 
+(defn get-lines [str]
+  "Returns a vector containing the lines split by \newline"
+  (vec (clojure.string/split str #"\n")))
+
+(defn get-line-n [coll,line-num]
+  (get coll line-num))
+
+(def indent-test (get-lines test-string-def))
+
+(get-line-n indent-test 2)
+
+(defn parse-code [coll]
+  (loop [x 0]
+  	(do 
+  		(if (= x (count coll))
+  			nil
+  		(if (= (count (re-seq #"\(" (get-line-n coll x))) 1)
+  				(println "Found beginin")
+  				(println "Comment or new line")
+  			))
+  	(recur (inc x))
+  	)))
+
+(parse-code indent-test)
+(defn count-lbrace [str]
+  (count #"\("))
+(defn count-rbrace [str]
+  (count #"\)"))
