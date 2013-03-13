34c75a94cab2b0ab6a8ccb3929dc5a885e748731
Emilian <bloodb0ne@yahoo.co.uk>
2013-03-13 11:41:05 +0200

    Added some code for function matching.

--- basic-checks.clj
+++ basic-checks.clj
@@ -8,3 +8,23 @@
  	   		(re-seq #"\[[\:\]\[\w\p{Space}\,]+\]" xs))
        (map (fn [x] (has-letter "," x)) 
  	   		(re-seq #"\([\:\]\)\w\p{Space}\,]+\)" xs))))
+
+;Defn matching
+;can be applied to other functions too
+(def test-string-def ";; good
+(defn foo
+  [x]
+  (bar x))
+
+;; good
+(defn foo [x]
+  (bar x))
+
+;; bad
+(defn foo
+  [x] (bar x))")
+
+(def def-test 
+	(re-seq #"\(defn[\:\w\[\]\(\p{Space})]+\)$" test-string-def))
+
+(first def-test)
