6c9a79aa6bbd91d71efa3f1b0b23f7e2f4afa66b
Emilian <bloodb0ne@yahoo.co.uk>
2013-03-18 10:58:30 +0200

    More testing functions.
    Trying to separete the string/file in the best way to make some rule
    checking system.

--- basic-checks.clj
+++ basic-checks.clj
@@ -1,29 +1,29 @@
 (defn has-letter [letter str] 
-  (> (count (re-seq  (re-pattern letter) 
-    str)) 
-	    0))
+	(> (count (re-seq  (re-pattern letter) 
+		str)) 
+			0))
 
 (defn comma-check [xs]
  (into (map (fn [x] (has-letter "," x)) 
- 	   		(re-seq #"\[[\:\]\[\w\p{Space}\,]+\]" xs))
-       (map (fn [x] (has-letter "," x)) 
- 	   		(re-seq #"\([\:\]\)\w\p{Space}\,]+\)" xs))))
+				(re-seq #"\[[\:\]\[\w\p{Space}\,]+\]" xs))
+			 (map (fn [x] (has-letter "," x)) 
+				(re-seq #"\([\:\]\)\w\p{Space}\,]+\)" xs))))
 
 ;Defn matching
 ;can be applied to other functions too
 (def test-string-def ";; good
 (defn foo
-  [x]
-  (bar x))
+	[x]
+	(bar x))
 
 ;; good
 (defn foo [x]
-  (bar x))
+	(bar x))
 (derper? foo
-  (bar x))
+	(bar x))
 ;; bad
 (defn foo
-  [x] (bar x))")
+	[x] (bar x))")
 
 (def def-test 
 	(re-seq #"\(defn[\:\w\[\]\(\p{Space})]+" test-string-def))
@@ -33,30 +33,62 @@
 
 ;test 2 
 (defn get-lines [str]
-  "Returns a vector containing the lines split by \newline"
-  (vec (clojure.string/split str #"\n")))
+	"Returns a vector containing the lines split by \newline"
+	(vec (clojure.string/split str #"\n")))
 
 (defn get-line-n [coll,line-num]
-  (get coll line-num))
+	(get coll line-num))
 
 (def indent-test (get-lines test-string-def))
 
 (get-line-n indent-test 2)
 
 (defn parse-code [coll]
-  (loop [x 0]
-  	(do 
-  		(if (= x (count coll))
-  			nil
-  		(if (= (count (re-seq #"\(" (get-line-n coll x))) 1)
-  				(println "Found beginin")
-  				(println "Comment or new line")
-  			))
-  	(recur (inc x))
-  	)))
+	(loop [x 0]
+		(do 
+			(if (= x (count coll))
+				nil
+			(if (= (count (re-seq #"\(" (get-line-n coll x))) 1)
+					(println (re-seq #"\([\w]+" (get-line-n coll x)))
+					(println "\n")
+				))
+		(recur (inc x))
+		)))
 
-(parse-code indent-test)
+;(parse-code indent-test)
+(def test-string-fun "(defn foo
+  [x] (bar (food x))))")
+
+;;Reformat this function 
+(defn read-function [x string]
+  (loop [ind x leftb 1 rightb 0 value '[\(]]
+    (let [character (get string ind)]
+    (cond
+      (= leftb rightb) {:start x :end ind :val (apply str value)}
+      (= character \() (recur (inc ind) (inc leftb) rightb (conj value character)) ;left brace
+      (= character \)) (recur (inc ind) leftb (inc rightb) (conj value character)) ;right brace 
+      :else (recur (inc ind) leftb rightb (conj value character))
+  ))))
+
+(read-function 1 test-string-fun)
+
+(defn parse-code-ver2 [string]
+	(loop [x 0]
+		(if (= x (count string))
+			:End
+			(do
+      (if (= (get string x) \( ) 
+        (do (prn (:val (read-function (+ x 1) string)))
+          (recur (:end (read-function (+ x 1) string)))) 
+           (recur (inc x))
+      )))))
+
 (defn count-lbrace [str]
-  (count #"\("))
+	(count #"\("))
 (defn count-rbrace [str]
-  (count #"\)"))
+	(count #"\)"))
+
+
+
+(parse-code-ver2 test-string-def)
+;(if (= (get str x) \; ) (prn "Brace" x) (println "a"))
